165710f0edab211a05ed568a3361ce8ff61bcf41
Santeri Rönkkö <santeri.ronkko@codemate.com>
2020-07-09 13:09:03 +0700

    fix: pluck functions typings

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,5 +1,5 @@
 export const flatten = (arr: any[]): any[] =>
   arr.reduce((acc, cur): any[] => acc.concat(Array.isArray(cur) ? flatten(cur) : cur), []);
 
-export const pluck = <T, K>(prop: keyof T, arr: T[]): K[] =>
-  arr.reduce<K[]>((acc, cur) => (!acc.includes(cur[prop] as any) ? acc.concat(cur[prop] as any) : acc), []);
+export const pluck = <T, K extends keyof T>(prop: K, arr: T[] | null | undefined): T[K][] =>
+  arr ? arr.reduce<T[K][]>((acc, cur) => (!acc.includes(cur[prop]) ? acc.concat(cur[prop]) : acc), []) : [];
