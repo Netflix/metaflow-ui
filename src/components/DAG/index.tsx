546105a1e12b423a04bbae8224092694810e83aa
Santeri Rönkkö <santeri.ronkko@codemate.com>
2020-07-13 16:04:53 +0700

    chore: remove react-flow-chart component

--- src/components/DAG/index.tsx
+++ src/components/DAG/index.tsx
@@ -1,107 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-import {
-  FlowChartWithState,
-  INodeDefaultProps,
-  INodeInnerDefaultProps,
-  ICanvasInnerDefaultProps,
-  ICanvasOuterDefaultProps,
-  INode,
-  IChart,
-} from '@mrblenny/react-flow-chart';
-
-import {
-  DAGModel,
-  convertDAGModelToIChart,
-  defaultChartConfig,
-  convertDAGModelToTree,
-  DAGStructureTree,
-  DAGTreeNode,
-  StepTree,
-} from './DAGUtils';
+import { DAGModel, convertDAGModelToTree, DAGStructureTree, DAGTreeNode, StepTree } from './DAGUtils';
 import { Run, Step } from '../../types';
 import { dagexample1, dagexample2, dagexample3, dagHugeflow } from './DAGexamples';
 import styled from 'styled-components';
 import Button from '../Button';
 import useResource from '../../hooks/useResource';
-
-//
-// Node
-//
-
-const Box = styled.div<{
-  node: INode;
-}>`
-  position: absolute;
-  padding: 10px;
-  color: white;
-  border-radius: 10px;
-  background: ${(props) => (props.node.properties?.type === 'loop' ? 'green' : '#3e3e3e')};
-
-  ${(props) =>
-    props.node.properties?.node_type &&
-    props.node.properties.node_type === 'container' &&
-    `
-    width: ${props.node.size?.width}px;
-    height: ${props.node.size?.height}px;
-    background: transparent;
-    border: 3px dashed ${props.node.properties?.container_type === 'foreach' ? 'green' : 'blue'};
-    opacity: 0.8;
-  `};
-
-  ${(props) =>
-    props.node.properties?.node_type &&
-    props.node.properties.node_type === 'normal' &&
-    `
-    width: 150px;
-    height: 100px;
-  `};
-`;
-
-const NodeCustom = React.forwardRef(
-  ({ node, children, ...otherProps }: INodeDefaultProps, ref: React.Ref<HTMLDivElement>) => {
-    return (
-      <Box ref={ref} node={node} {...otherProps}>
-        {children}
-      </Box>
-    );
-  },
-);
-
-const NodeInnerCustom = React.forwardRef((props: INodeInnerDefaultProps, ref: React.Ref<HTMLDivElement>) => {
-  return (
-    <div ref={ref}>
-      <div>{props.node.properties.step_name}</div>
-      <div>{props.node.properties.original?.box_ends}</div>
-    </div>
-  );
-});
-
-//
-// Canvas
-//
-
-export const CanvasOuterDefault = styled.div<ICanvasOuterDefaultProps>`
-  position: relative;
-  width: 100%;
-  overflow: hidden;
-  cursor: not-allowed;
-`;
-
-const CanvasOuterCustom = React.forwardRef((props: ICanvasOuterDefaultProps, ref: React.Ref<HTMLDivElement>) => {
-  return <CanvasOuterDefault {...props} ref={ref} />;
-});
-
-export const CanvasInnerStyle = styled.div<ICanvasInnerDefaultProps>`
-  position: relative;
-  width: 1000px;
-  height: 1000px;
-  cursor: move;
-`;
-
-const CanvasInnerCustom = React.forwardRef((props: ICanvasInnerDefaultProps, _) => {
-  return <CanvasInnerStyle {...(props as any)} />;
-});
 
 //
 // DAG
@@ -131,24 +35,15 @@ const DAG: React.FC<{ run: Run }> = ({ run }) => {
   });
 
   const [dagTree, setDagTree] = useState<DAGStructureTree>([]);
-  const [dagChart, setChart] = useState<IChart | null>(null);
   const [dataSet, setDataSet] = useState<DAGModel | null>(null);
-  const [mode, setmode] = useState<'fancy' | 'simple'>('simple');
 
   useEffect(() => {
     if (dataSet) {
-      if (mode === 'simple') {
-        setDagTree(convertDAGModelToTree(dataSet));
-      } else {
-        setChart(null);
-        setTimeout(() => {
-          setChart(convertDAGModelToIChart(dataSet));
-        }, 1);
-      }
+      setDagTree(convertDAGModelToTree(dataSet));
     } else {
       setDagTree(convertDAGModelToTree(dagHugeflow));
     }
-  }, [dataSet, mode]);
+  }, [dataSet]);
 
   return (
     <div>
@@ -157,24 +52,9 @@ const DAG: React.FC<{ run: Run }> = ({ run }) => {
         <Button onClick={() => setDataSet(dagexample2)}>example2</Button>
         <Button onClick={() => setDataSet(dagexample3)}>example3</Button>
         <Button onClick={() => setDataSet(dagHugeflow)}>hugeflow</Button>
-        <label>Rendering mode</label>
-        <Button onClick={() => setmode('fancy')}>fancy{mode === 'fancy' ? ' ✓' : ''}</Button>
-        <Button onClick={() => setmode('simple')}>simple{mode === 'simple' ? ' ✓' : ''}</Button>
       </div>
-      {dagChart && mode === 'fancy' && (
-        <FlowChartWithState
-          initialValue={dagChart}
-          config={defaultChartConfig.config}
-          Components={{
-            Node: NodeCustom,
-            NodeInner: NodeInnerCustom,
-            CanvasInner: CanvasInnerCustom,
-            CanvasOuter: CanvasOuterCustom,
-          }}
-        />
-      )}
 
-      {dagTree && mode === 'simple' && (
+      {dagTree && (
         <DAGRenderingContainer>
           <div style={{ display: 'flex' }}>
             <NormalItemContainer>
