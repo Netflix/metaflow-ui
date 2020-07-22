daeb6424567437293d4d969297a92514b8e3eda6
Teemu Alapoikela <teemu.alapoikela@gmail.com>
2020-07-22 16:56:24 +0300

    Fixed sidebar on the front page

--- src/components/Structure/index.tsx
+++ src/components/Structure/index.tsx
@@ -5,21 +5,6 @@ export const Page = styled.div`
   padding: 0 ${(p) => p.theme.layout.pagePaddingX}rem ${(p) => p.theme.layout.pagePaddingY}rem;
 `;
 
-export const Layout = styled.main`
-  display: flex;
-  justify-content: space-between;
-`;
-
-export const Sidebar = styled.div`
-  flex: 0 0 ${(p) => p.theme.layout.sidebarWidth}rem;
-  margin-right: ${(p) => p.theme.spacer.hg}rem;
-`;
-
-export const Content = styled.div`
-  flex: 1;
-  max-width: 100%;
-`;
-
 export const Section = styled.section`
   padding: 0 ${(p) => p.theme.spacer.sm}rem;
   margin-bottom: ${(p) => p.theme.spacer.md}rem;
