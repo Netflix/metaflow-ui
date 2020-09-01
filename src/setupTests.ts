d1adb6f21ccb636a693d77be0e608028c99a5e9d
Santeri Rönkkö <santeri.ronkko@codemate.com>
2020-09-01 11:42:59 +0700

    test: Update tests to succeed

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -6,3 +6,23 @@ import '@testing-library/jest-dom/extend-expect';
 
 const noop = () => null;
 Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
+
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
