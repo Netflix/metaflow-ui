fb04b5af8abdf3152dcbc9aa9d876decc7f02c63
Santeri Rönkkö <santeri.ronkko@codemate.com>
2020-09-23 16:36:53 +0700

    feat: Replace taginput with new filter input

---
+++ src/components/FilterInput/index.tsx
@@ -0,0 +1,88 @@
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components';
+import Button from '../Button';
+import Icon from '../Icon';
+import { SectionHeader } from '../Structure';
+
+const FilterInput: React.FC<{ onSubmit: (k: string) => void; sectionLabel: string }> = ({ onSubmit, sectionLabel }) => {
+  const [hasFocus, setHasFocus] = useState(false);
+  const [val, setVal] = useState('');
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  return (
+    <FilterInputWrapper active={hasFocus}>
+      <div style={{ position: 'relative' }}>
+        <input
+          ref={inputEl}
+          placeholder={sectionLabel}
+          value={val}
+          onKeyPress={(e) => {
+            if (e.charCode === 13 && e.currentTarget.value) {
+              onSubmit(e.currentTarget.value);
+              setVal('');
+            }
+          }}
+          onChange={(e) => {
+            setVal(e.currentTarget.value);
+          }}
+          onFocus={() => {
+            setHasFocus(true);
+          }}
+          onBlur={() => {
+            setHasFocus(false);
+          }}
+        />
+        <SubmitIconHolder
+          focus={hasFocus}
+          onClick={() => {
+            if (inputEl?.current?.value) {
+              onSubmit(inputEl.current.value);
+              setVal('');
+            }
+          }}
+        >
+          <Icon name="enter" size="xs" />
+        </SubmitIconHolder>
+      </div>
+      <Button
+        iconOnly
+        onClick={() => {
+          if (!hasFocus && inputEl?.current) {
+            inputEl.current.focus();
+          } else if (hasFocus && inputEl?.current?.value) {
+            onSubmit(inputEl.current.value);
+          }
+        }}
+      >
+        <Icon name={hasFocus ? 'times' : 'plus'} />
+      </Button>
+    </FilterInputWrapper>
+  );
+};
+
+const FilterInputWrapper = styled(SectionHeader)<{ active: boolean }>`
+  padding-bottom: 0.375rem;
+
+  ${(p) => (p.active ? `border-bottom-color: ${p.theme.color.text.blue};` : '')}
+
+  input {
+    border: none;
+    &:focus {
+      outline: none;
+      border: none;
+    }
+  }
+`;
+
+const SubmitIconHolder = styled.div<{ focus: boolean }>`
+  position: absolute;
+  right: 0;
+  top: 0;
+  line-height: 26px;
+  opacity: ${(p) => (p.focus ? 1 : 0)};
+  transition: opacity 0.15s;
+  cursor: ${(p) => (p.focus ? 'pointer' : 'normal')};
+  margin-right: 2px;
+`;
+
+export default FilterInput;
