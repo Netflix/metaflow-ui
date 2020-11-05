f02647901f95c16522852602926dcf7af5c8a821
Santeri Rönkkö <santeri.ronkko@codemate.com>
2020-11-05 11:20:41 +0700

    feat: Preserve search field state in url

--- src/hooks/useSearchField/index.ts
+++ src/hooks/useSearchField/index.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { StringParam, useQueryParams } from 'use-query-params';
 import useSearchRequest, { SearchResult, TaskMatch } from '../useSearchRequest';
 
 export type SearchResultModel = {
@@ -24,7 +25,8 @@ function isCached(flowId: string, runNumber: string) {
 }
 
 export default function useSeachField(flowID: string, runNumber: string): SearchFieldReturnType {
-  const [searchValue, setSearchValue] = useState(isCached(flowID, runNumber) ? cache.text : '');
+  const [qp, setQp] = useQueryParams({ q: StringParam });
+  const [searchValue, setSearchValue] = useState(qp.q ? qp.q : isCached(flowID, runNumber) ? cache.text : '');
   const [searchResults, setSearchResults] = useState<SearchResultModel>(
     isCached(flowID, runNumber) ? cache.results : { result: [], status: 'NotAsked' },
   );
@@ -37,6 +39,7 @@ export default function useSeachField(flowID: string, runNumber: string): Search
 
   const updateText = (str: string) => {
     setSearchValue(str);
+    setQp({ q: str });
     cache.text = str;
     cache.id = flowID + runNumber;
   };
@@ -64,6 +67,13 @@ export default function useSeachField(flowID: string, runNumber: string): Search
       setSearchResults({ result: [], status: 'NotAsked' });
     }
   }, [searchValue, setSearchResults]);
+
+  useEffect(() => {
+    if (qp.q) {
+      cache.text = qp.q;
+      cache.id = flowID + runNumber;
+    }
+  }, []); // eslint-disable-line
 
   return {
     results: searchResults,
