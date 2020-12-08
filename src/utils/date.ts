ae1eaa2f9fbbd44b4cc399521e814e26bb6dc088
Santeri Rönkkö <santeri.ronkko@codemate.com>
2020-12-08 11:24:53 +0700

    chore: Remove console logs

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,8 +2,6 @@ import spacetime from 'spacetime';
 
 export const getISOString = (date: Date, timezone?: string): string => {
   if (timezone) {
-    console.log(timezone);
-
     return spacetime(date, 'GMT+0').goto(`${timezone}`).unixFmt('MM-dd-yyyy HH:mm');
   }
   return spacetime(date, 'GMT+0').unixFmt('MM-dd-yyyy HH:mm');
