d312603e8ea60be6f06e1cb4255ea10b742665f6
Santeri Rönkkö <santeri.ronkko@codemate.com>
2021-02-03 12:23:05 +0700

    fix/sort-arrows-were-wrong-way

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,14 +1,14 @@
 export type DirectionText = 'down' | 'up';
 export type DirectionSymbolic = '+' | '-';
 
-export const parseDirection = (dir: DirectionSymbolic): DirectionText => (dir === '+' ? 'down' : 'up');
+export const parseDirection = (dir: DirectionSymbolic): DirectionText => (dir === '+' ? 'up' : 'down');
 
 export const parseOrderParam = (val: string): [DirectionText, string] => {
   const dir = val.substr(0, 1) as DirectionSymbolic;
   return [parseDirection(dir), val.substr(1)];
 };
 
-export const directionFromText = (text: DirectionText): DirectionSymbolic => (text === 'down' ? '+' : '-');
+export const directionFromText = (text: DirectionText): DirectionSymbolic => (text === 'up' ? '+' : '-');
 
 export const swapDirection = (param: string): string => {
   const [dir, prop] = parseOrderParam(param);
