1af1f97c66beec05feeece8ee33aa80ade9e8715
Santeri Rönkkö <santeri.ronkko@codemate.com>
2021-03-03 17:27:12 +0700

    tests: Add tests to TitledRow

---
+++ src/components/TitledRow/__tests__/TitledRow.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import TestWrapper from '../../../utils/testing';
+import TitledRow, { valueToRenderableType } from '..';
+
+test('renderValue', () => {
+  // React node
+  expect(valueToRenderableType(<h1>hello</h1>)).toEqual(<h1>hello</h1>);
+  // boolean
+  expect(valueToRenderableType(true)).toBe('True');
+  expect(valueToRenderableType(false)).toBe('False');
+  // number
+  expect(valueToRenderableType(1)).toBe(1);
+  expect(valueToRenderableType(1.1231)).toBe(1.1231);
+  // string
+  expect(valueToRenderableType('1')).toBe('1');
+  expect(valueToRenderableType('hello world')).toBe('hello world');
+  // stringified object
+  expect(valueToRenderableType('{"a":"hello world"}')).toBe('{\n  "a": "hello world"\n}');
+  expect(valueToRenderableType('{"a":"h","b":4,"c":{"a":"a"}}')).toBe(
+    '{\n  "a": "h",\n  "b": 4,\n  "c": {\n    "a": "a"\n  }\n}',
+  );
+  // object
+  expect(valueToRenderableType({ a: 'hello world' })).toBe('{\n  "a": "hello world"\n}');
+  expect(valueToRenderableType({ a: 'h', b: 4, c: { a: 'a' } })).toBe(
+    '{\n  "a": "h",\n  "b": 4,\n  "c": {\n    "a": "a"\n  }\n}',
+  );
+  // array
+  expect(valueToRenderableType([1, 2, 3, 4])).toBe('[\n  1,\n  2,\n  3,\n  4\n]');
+});
+
+test('<TitledRow /> - default mode', () => {
+  const { getByTestId } = render(
+    <TestWrapper>
+      <TitledRow type="default" title="Hello" content={<h1>hello world</h1>} />
+    </TestWrapper>,
+  );
+
+  expect(getByTestId('titled-row-title').textContent).toBe('Hello');
+  expect(getByTestId('titled-row-default-mode').textContent).toBe('hello world');
+});
+
+test('<TitledRow /> - table mode', () => {
+  const { getAllByTestId } = render(
+    <TestWrapper>
+      <TitledRow type="table" title="Hello" content={{ 'react-node': <h1>hello world</h1>, booleanvalue: true }} />
+    </TestWrapper>,
+  );
+  expect(getAllByTestId('titledrow-row-title')[0].textContent).toBe('react-node');
+  expect(getAllByTestId('titledrow-row-value')[0].textContent).toBe('hello world');
+  expect(getAllByTestId('titledrow-row-title')[1].textContent).toBe('booleanvalue');
+  expect(getAllByTestId('titledrow-row-value')[1].textContent).toBe('True');
+});
