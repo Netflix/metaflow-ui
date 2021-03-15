c56a15e891b44a6e91afa264ff9ebb5420254811
Santeri Rönkkö <11741291+rsanteri@users.noreply.github.com>
2021-03-15 17:03:45 +0700

    chore: Remove unused service worker files (#324)

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './utils/i18n';
-import * as serviceWorker from './serviceWorker';
 import './utils/VERSION';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
