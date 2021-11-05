54c6feffd7617d87d2fce10c779a64522e21c6d5
Sakari Ikonen <sakari.ikonen@codemate.com>
2021-11-05 14:04:16 +0200

    remove unused error translation

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -234,7 +234,6 @@ const en = {
       'stack-trace-copied': 'Stack trace copied',
       'download-stack-trace': 'Download stack trace',
 
-      'failed-to-load-dag': 'Failed to load DAG.',
       DataException: 'There was a problem accessing data with the Metaflow client.',
       MetaflowS3AccessDenied: 'Access denied. There was a problem with AWS credentials.',
       MetaflowS3NotFound: 'S3 bucket was not found.',
