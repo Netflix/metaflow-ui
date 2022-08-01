5c0ef964c89f1974a368c6f4fd660a3f0c13871f
Brendan Gibson <brendan@outerbounds.co>
2022-08-01 20:58:46 +1000

    Added postMessage comms with iframe

--- src/components/MFCard/CardIframe.tsx
+++ src/components/MFCard/CardIframe.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import { MESSAGE_NAME } from '../Plugins/PluginManager';
 import { apiHttp } from '../../constants';
 
 const CHECK_HEIGHT_INTERVAL = 1000;
@@ -7,6 +8,8 @@ const CHECK_HEIGHT_INTERVAL = 1000;
 type Props = {
   path: string;
 };
+
+const FALLBACK_HEIGHT = 150;
 
 //
 // Render single card in iframe.
@@ -18,15 +21,49 @@ const CardIframe: React.FC<Props> = ({ path }) => {
 
   // Check iframe height every second in case it changes somehow.
   useEffect(() => {
-    setInterval(() => {
-      if (ref.current) {
-        const body = ref.current.contentWindow?.document.body;
-        const h = Math.max(body?.scrollHeight ?? 0, body?.clientHeight ?? 0, body?.offsetHeight ?? 0);
-        if (h) {
-          setElementHeight(h);
+    let body: HTMLElement | undefined;
+
+    // Listen for a message from the iframe to check the height.
+    const listener = (e: MessageEvent) => {
+      if (e.data.type === MESSAGE_NAME.HEIGHT_CHECK) {
+        if (typeof e.data.height === 'number') {
+          // Stop checking iframe periodically if it tells us what height it is.
+          clearInterval(interval);
+          setElementHeight(e.data.height);
+        } else {
+          console.warn("Height assign request didn't have a height value");
         }
       }
-    }, CHECK_HEIGHT_INTERVAL);
+    };
+
+    // Check to see if security allows access to the iframe (same domain).
+    // If so, check the height every second
+    // If not, wait for a postMessage from the iframe to set the height.
+    const checkHeight = () => {
+      try {
+        body = ref.current?.contentWindow?.document.body;
+        if (body) {
+          const h = Math.max(body?.scrollHeight ?? 0, body?.clientHeight ?? 0, body?.offsetHeight ?? 0);
+          if (h) {
+            setElementHeight(h);
+          } else {
+            setElementHeight(FALLBACK_HEIGHT);
+          }
+        }
+      } catch (e) {
+        setElementHeight(FALLBACK_HEIGHT);
+      }
+    };
+
+    window.addEventListener('message', listener);
+
+    // Check iframe height every second.
+    const interval = setInterval(checkHeight, CHECK_HEIGHT_INTERVAL);
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener('message', listener);
+    };
   }, []);
 
   return (
