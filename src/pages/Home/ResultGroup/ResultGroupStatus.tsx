3a4cbd419013e95d2d298231636792396de81512
Brendan Gibson <brendan@outerbounds.co>
2023-08-23 08:21:41 -0600

    Added tooltip to run status

--- src/pages/Home/ResultGroup/ResultGroupStatus.tsx
+++ src/pages/Home/ResultGroup/ResultGroupStatus.tsx
@@ -4,6 +4,7 @@ import { colorByStatus, iconByStatus } from '../../../utils/style';
 import Icon, { SupportedIcons } from '../../../components/Icon';
 import React from 'react';
 import { RunStatus } from '../../../types';
+import { SmallText } from '../../../components/Text';
 
 //
 // Small status indicator on left side of the rows
@@ -16,18 +17,45 @@ type StatusColorCellProps = {
 
 const StatusColorCell: React.FC<StatusColorCellProps> = ({ status, title }) => {
   const name: keyof SupportedIcons | undefined = iconByStatus(status);
-  console.log('name', name, title);
-  return <StatusCell status={status}>{name && <Icon name={name} size="xs" />}</StatusCell>;
+
+  return (
+    <>
+      <StatusCell status={status}>
+        {name && <Icon name={name} size="xs" />}
+        <Text>{title} Run</Text>
+      </StatusCell>
+    </>
+  );
 };
 
 const StatusCell = styled(TD)<{
   status: string;
 }>`
   text-align: center;
+  position: relative;
   color: ${(p) => colorByStatus(p.theme, p.status) || 'transparent'} !important;
   i {
     vertical-align: middle;
   }
+  :hover small {
+    opacity: 1;
+  }
+`;
+
+const Text = styled(SmallText)`
+  white-space: 'nowrap';
+  transition: opacity 0.25s;
+  opacity: 0;
+  position: absolute;
+  left: 50%;
+  top: 75%;
+  color: ${(p) => p.theme.color.text.white};
+  background: ${(p) => p.theme.color.bg.tooltip};
+  border-radius: 6px;
+  padding: 0.5rem 0.75rem;
+  white-space: nowrap;
+  text-transform: capitalize;
+  z-index: 10;
 `;
 
 export default StatusColorCell;
