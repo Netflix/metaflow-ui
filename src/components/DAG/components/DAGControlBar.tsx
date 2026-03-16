610ad60fe34237f39b1d8be8f0cb4e9d82b7e41f
Vinyl-Davyl <okononfuadavid@gmail.com>
2026-03-16 08:14:13 +0100

    feat: Expanded DAG Mode implementation, high-density monitoring capabilities to the DAG visualization

--- src/components/DAG/components/DAGControlBar.tsx
+++ src/components/DAG/components/DAGControlBar.tsx
@@ -1,5 +1,6 @@
 import { TFunction } from 'i18next';
 import React from 'react';
+import styled from 'styled-components';
 import Button from '@components/Button';
 import Icon from '@components/Icon';
 import { ItemRow } from '@components/Structure';
@@ -10,16 +11,36 @@ import { ItemRow } from '@components/Structure';
 
 type DAGControlBarProps = {
   setFullscreen: (v: boolean) => void;
+  isExpanded: boolean;
+  setExpanded: (v: boolean) => void;
   t: TFunction;
 };
 
-const DAGControlBar: React.FC<DAGControlBarProps> = ({ setFullscreen, t }) => (
+const DAGControlBar: React.FC<DAGControlBarProps> = ({ setFullscreen, isExpanded, setExpanded, t }) => (
   <ItemRow pad="sm" justify="flex-end">
-    <Button onClick={() => setFullscreen(true)} withIcon data-testid="dag-control-fullscreen-button">
-      <Icon name="maximize" />
-      <span>{t('run.show-fullscreen') as string}</span>
-    </Button>
+    <ControlBarItem>
+      <Button
+        onClick={() => setExpanded(!isExpanded)}
+        withIcon
+        active={isExpanded}
+        data-testid="dag-control-expand-button"
+      >
+        <Icon name={isExpanded ? 'collapse' : 'expand'} />
+        <span>{t(isExpanded ? 'run.collapse-dag' : 'run.expand-dag') as string}</span>
+      </Button>
+    </ControlBarItem>
+
+    <ControlBarItem>
+      <Button onClick={() => setFullscreen(true)} withIcon data-testid="dag-control-fullscreen-button">
+        <Icon name="maximize" />
+        <span>{t('run.show-fullscreen') as string}</span>
+      </Button>
+    </ControlBarItem>
   </ItemRow>
 );
+
+const ControlBarItem = styled.div`
+  margin-left: 0.5rem;
+`;
 
 export default DAGControlBar;
